95b08276be8786013a58bef67c07fb3527caf1aa
Charles Foy <clsfoy@gmail.com>
2020-12-04 14:00:21 +0000

    formatted css for add student page

--- src/Components.js/AddStudent.js
+++ src/Components.js/AddStudent.js
@@ -5,10 +5,17 @@ import Nav from "./Nav";
 class AddStudent extends Component {
   render() {
     return (
-      <div>
+      <div className="form">
         <Header></Header>
         <h3>Add Students</h3>
         <Nav></Nav>
+        <div className="form-container">
+          <form className="form-input">
+            <label>
+              Name: <input type="text"></input>
+            </label>
+          </form>
+        </div>
       </div>
     );
   }
